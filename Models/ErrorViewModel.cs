e35f071b5d0e3643168a57d35a1b36957e76609d
MaarufB <maarufburad1231@gmail.com>
2022-01-15 00:16:40 +0800

    initial build added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ezCloth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
